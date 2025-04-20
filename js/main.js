998708bc64437848491687266584696c2d972973
SomethingW1tt4 <jasongberger@gmail.com>
2025-04-20 00:15:50 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -93,3 +93,12 @@
     
 })(jQuery);
 
+document.querySelectorAll('.gallery-item video').forEach(video => {
+    video.addEventListener('mouseenter', () => {
+        video.play();
+    });
+    video.addEventListener('mouseleave', () => {
+        video.pause();
+        video.currentTime = 0; // Reset to start
+    });
+});
