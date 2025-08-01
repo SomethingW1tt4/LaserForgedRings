e2c099bb65e5ae5f5ef53d87021f43fcc2e88e5b
SomethingW1tt4 <jasongberger@gmail.com>
2025-08-01 16:16:50 -0700

    trying copilot's solution

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@
 
 
     // Initiate the wowjs
-   new WOW().init();
+    new WOW().init();
 
     // Lazy load videos with data-src
     $('video[data-src]').each(function () {
@@ -34,25 +34,30 @@
         observer.observe(video);
     });
 
+    
     // Sticky Navbar
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('bg-primary shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('bg-primary shadow-sm').css('top', '-150px');
-        }
-    });
+  if ($(window).width() <= 576) {
+    if ($(this).scrollTop() > 300) {
+      $('.navbar').removeClass('hide-on-scroll');
+    } else {
+      $('.navbar').addClass('hide-on-scroll');
+    }
+  }
+});
 
 
     // Back to top button
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
+        if ($(window).width() <= 576) {
+            if ($(this).scrollTop() > 300) {
+                $('.navbar').css('top', '0px');
+            } else {
+                $('.navbar').css('top', '-150px');
+            }
         }
     });
-    
+
     $('.back-to-top').click(function () {
         $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
