02fdc37bc6af03ce3a8b4d04b95c69b165cc1bcd
SomethingW1tt4 <jasongberger@gmail.com>
2025-08-02 12:23:47 -0700

    doubling versions for all pages, compare final touches

--- js/main.js
+++ js/main.js
@@ -30,15 +30,6 @@
 
         lastScrollTop = currentScroll;
     });
-    /*
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('bg-primary shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('bg-primary shadow-sm').css('top', '-150px');
-        }
-    });
-    */
 
     // Back to top button
     $(window).scroll(function () {
