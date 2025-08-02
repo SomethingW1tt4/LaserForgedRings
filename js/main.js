0282ab50a9b2b702cde2165f7853aa43b935335a
SomethingW1tt4 <jasongberger@gmail.com>
2025-08-01 23:19:17 -0700

    js commenting out stickynavbar

--- js/main.js
+++ js/main.js
@@ -17,14 +17,14 @@
 
 
     // Sticky Navbar
-    $(window).scroll(function () {
+   /* $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('.sticky-top').addClass('bg-primary shadow-sm').css('top', '0px');
         } else {
             $('.sticky-top').removeClass('bg-primary shadow-sm').css('top', '-150px');
         }
     });
-    
+    */
     
     // Back to top button
     $(window).scroll(function () {
